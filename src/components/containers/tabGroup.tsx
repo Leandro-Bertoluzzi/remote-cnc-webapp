640dcfc19cbbf359aff007919fa5764a447673b6
Leandro Bertoluzzi <leajb10@gmail.com>
2024-06-11 19:19:37 -0300

    feat: Added hook for screen size, made tab group responsive

--- src/components/containers/tabGroup.tsx
+++ src/components/containers/tabGroup.tsx
@@ -1,10 +1,12 @@
-import { Tabs } from "flowbite-react";
+import { Accordion, Tabs } from "flowbite-react";
 import TabGroupProps from "@/types/TabGroupProps";
+import useScreenSize from "@/hooks/useScreenSize";
 
 export default function TabGroup(props: TabGroupProps) {
+    const screenSize = useScreenSize();
     const { items } = props;
 
-    return (
+    return screenSize.width > 768 ? (
         <Tabs aria-label="Default tabs" style="default">
             {items.map((item) => (
                 <Tabs.Item key={item.title} title={item.title} icon={item.icon}>
@@ -12,5 +14,14 @@ export default function TabGroup(props: TabGroupProps) {
                 </Tabs.Item>
             ))}
         </Tabs>
+    ) : (
+        <Accordion>
+            {items.map((item) => (
+                <Accordion.Panel key={item.title}>
+                    <Accordion.Title>{item.title}</Accordion.Title>
+                    <Accordion.Content>{item.content}</Accordion.Content>
+                </Accordion.Panel>
+            ))}
+        </Accordion>
     );
 }
