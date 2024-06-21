cdb6293580913a7eb45ea6e6f9a8b8005d973c69
Leandro Bertoluzzi <leajb10@gmail.com>
2024-06-21 17:06:22 -0300

    refactor: Removed redundant select component

--- src/types/LabeledSelectProps.tsx
+++ src/types/LabeledSelectProps.tsx
@@ -1,9 +1,14 @@
 type btnActionType = (value: string) => void;
 
+interface SelectOption {
+    label: string;
+    value: string | number;
+}
+
 export default interface LabeledSelectProps {
     handleChange: btnActionType;
     name: string;
     label: string;
-    options: string[];
-    selectedOption?: string;
+    options: SelectOption[];
+    initialValue?: string | number;
 }
